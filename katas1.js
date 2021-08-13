96e3afa7fb85f3cb24119f7e677ca1f1bd6b41cd
Aine Mota <motaaine1@gmail.com>
2021-08-13 20:26:19 -0300

    Adicionei return ao fim das funções

--- katas1.js
+++ katas1.js
@@ -1,39 +1,45 @@
-document.write('Retorno de 1 a 20 <br>');
+
 function oneThroughTwenty() {
+
     let n = 1;
+    let ate20 = [];
 
    while(n <= 20){
-    document.write( n + '<br>');
+    ate20.push(n);
     n++;
    }
+   return ate20;
 }
 
 
 oneThroughTwenty();
 
-document.write('Retorno de pares de 1 a 20 <br>')
+
 
 function evensToTwenty() {
    
   let counter = 2;
+  let pares = [];
   
    while(counter <= 20) {
-     document.write(counter + '<br>');
+     pares.push(counter);
      counter += 2;
 
    }
+   return pares;
 }
 
 evensToTwenty();
 
-document.write('Retorno de ímpares de 1 a 20<br>')
+
 function oddsToTwenty() {
     
   let counter = 1;
+  let impares = [];
 
   while(counter <= 20) {
 
-    document.write(counter + '<br>');
+    impares.push(counter);
     counter += 2;
   }
    
@@ -42,86 +48,103 @@ function oddsToTwenty() {
 oddsToTwenty();
 
 
-document.write('Retorno multiplos de 5 até 100 <br>')
+
 function multiplesOfFive() {
 
-  let multi = 0;
+  let fiveProducts = 0;
+  let multiplos = [];
 
 for (let counter = 1; multi <= 100;counter++){
     
     multi = counter * 5;
-    document.write(multi + '<br>');
+    multiplos.push(multi);
+    
 }
+return multiplos;
  }
 
 
 multiplesOfFive();
 
-document.write('Retorno quadrados perfeitos até 100 <br>');
+
 function squareNumbers() {
-  let quadrado = 0; 
+
+  let potencia = 0; 
+  let valores = [];
 
    for(let n = 1; quadrado< 100; n++){
 
-      quadrado = n * n;  
-      document.write(quadrado + '<br>');
+      potencia = n ** 2;  
+      quadrados.push(quadrado);
   } 
+  return quadrados;
 }
 
 squareNumbers();
 
 
-document.write('Retorno contagem regressiva a partir de 20 <br>')
+
 function countingBackwards() {
+
     let n = 20
- while(n > 0){
-   document.write(n + '<br>');
+    let regressiva = [];
+ 
+    while(n > 0){
+
+   regressiva.push(n);
    n -= 1;
  }
+ return regressiva;
 }
 
 countingBackwards();
 
 
-document.write('Retorno regressivo de pares a partir de 20 <br>')
+
 function evenNumbersBackwards() {
     
  let n = 20;
+ let pares = [];
  
  while(n > 0){
-   document.write(n + '<br>');
+   pares.push(n);
    n -= 2;  
  }
+ return pares;
 }
 
 evenNumbersBackwards();
 
-document.write('Retorno regressivo de ímpares a partir de 20 <br>')
+
 function oddNumbersBackwards() {
     
  let n = 19;
+ let impares = [];
   
  while(n > 0){
-   document.write(n + '<br>');
+   impares.push(n);
    n -= 2;
  }
+ return impares;
 }
 
 oddNumbersBackwards();
 
-document.write('Retorno regresivo dos multiplos de 5 a partir de 100 <br>')
+
 function multiplesOfFiveBackwards() {
   let n = 100;  
+  let multiFive = [];
   
   while(n > 0){
-    document.write(n + '<br>');
+    multiFive.push(n);
     n -= 5;
   }
+  return multiFive;
 }
 
 multiplesOfFiveBackwards();
 
-document.write('Retorno regressivo dos quadrados perfeitos a partir de 100 <br>')
+
 function squareNumbersBackwards() {
    let impares = [];
    let numero = 1;
@@ -133,9 +156,10 @@ function squareNumbersBackwards() {
    }
    for(let indice = (impares.length - 1); indice >= 0; indice--){
       reg -= impares[indice];
-     document.write( reg + '<br>');
+     
      
    }
+   return reg;
 }
 
 squareNumbersBackwards();
